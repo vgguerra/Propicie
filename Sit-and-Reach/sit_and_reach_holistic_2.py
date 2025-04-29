c8aa0d9d05e0f2796e5674967f1ff89421d200c0
vgguerra <victorevandro94@gmail.com>
2025-04-29 11:58:06 +0100

    Correções de variáveis

--- Sit-and-Reach/sit_and_reach_holistic_2.py
+++ Sit-and-Reach/sit_and_reach_holistic_2.py
@@ -233,19 +233,19 @@ def draw_angles_arcs(repeats,knee_angle, opposite_knee_angle, hip_angle, elbow_a
     opposite_elbow_coords = tuple(np.multiply(opposite_elbow[:2], [frame.shape[1], frame.shape[0]]).astype(int))
     opposite_wrist_coords = tuple(np.multiply(opposite_wrist[:2], [frame.shape[1], frame.shape[0]]).astype(int))
 
-    # cv2.putText(image, f'Opposite Knee Angle: {opposite_knee_angle:.2f}',(1000,400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
-    # cv2.putText(image, f'Opposite Elbow Angle: {opposite_elbow_angle:.2f}', opposite_elbow_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
+    cv2.putText(image, f'Opposite Knee Angle: {opposite_knee_angle:.2f}',(1000,400), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
+    cv2.putText(image, f'Opposite Elbow Angle: {opposite_elbow_angle:.2f}', opposite_elbow_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
 
-    # draw_dynamic_angle_arc(image,opposite_shoulder_coords,opposite_elbow_coords,opposite_wrist_coords,opposite_elbow_angle)
+    draw_dynamic_angle_arc(image,opposite_shoulder_coords,opposite_elbow_coords,opposite_wrist_coords,opposite_elbow_angle)
 
-    # draw_dynamic_angle_arc(image,hip_coords, knee_coords, ankle_coords, knee_angle)
-    # cv2.putText(image, f'Knee Angle: {knee_angle:.2f}', knee_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 230, 0), 2)
+    draw_dynamic_angle_arc(image,hip_coords, knee_coords, ankle_coords, knee_angle)
+    cv2.putText(image, f'Knee Angle: {knee_angle:.2f}', knee_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 230, 0), 2)
 
-    # draw_dynamic_angle_arc(image, shoulder_coords, hip_coords, knee_coords, hip_angle)
-    # cv2.putText(image, f'Hip Angle: {hip_angle:.2f}', hip_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
+    draw_dynamic_angle_arc(image, shoulder_coords, hip_coords, knee_coords, hip_angle)
+    cv2.putText(image, f'Hip Angle: {hip_angle:.2f}', hip_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
 
-    # draw_dynamic_angle_arc(image, shoulder_coords, elbow_coords, wrist_coords, elbow_angle)
-    # cv2.putText(image, f'Elbow Angle: {elbow_angle:.2f}', elbow_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
+    draw_dynamic_angle_arc(image, shoulder_coords, elbow_coords, wrist_coords, elbow_angle)
+    cv2.putText(image, f'Elbow Angle: {elbow_angle:.2f}', elbow_coords, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
 
 # Function to calculate all the angles needed to perform the exercise
 def calculate_angles(repeats, pose_landmarks):
@@ -351,8 +351,8 @@ def process_exercise(repeats):
                     distance = dist_pixels * PIXEL_TO_CM_RATIO  
 
 
-                    cv2.putText(image, f'Postion X and Y of foot: {foot[0]}, {foot[1]}',(1000,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
-                    cv2.putText(image, f'Position X and Y of hand: {hand[0]}, {hand[1]}',(1000,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
+                    # cv2.putText(image, f'Postion X and Y of foot: {foot[0]}, {foot[1]}',(1000,100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
+                    # cv2.putText(image, f'Position X and Y of hand: {hand[0]}, {hand[1]}',(1000,200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 235, 0), 2)
 
                     # Calculate average distance
                     distances.append(distance)
@@ -469,27 +469,27 @@ distances_left = []
 
 repeats = 0
 
-# idade,altura,peso,genero = register()
+idade,altura,peso,genero = register()
 
 while repeats < 4:
     final_distance = process_exercise(repeats)
 
     if final_distance is not None:
 
-        # real = real_distance()
-        # caminho_arquivo = "./tabelas/dados2.xlsx"
-        # df = pd.read_excel(caminho_arquivo, engine="openpyxl")
+        real = real_distance()
+        caminho_arquivo = "./tabelas/dados2.xlsx"
+        df = pd.read_excel(caminho_arquivo, engine="openpyxl")
 
-        # nova_linha = {
-        #     "Idade": idade,
-        #     "Altura": altura,
-        #     "Peso": peso,
-        #     "Gênero": genero,
-        #     "Distância real": real,
-        #     "Distância calculada": final_distance,
-        # }
-        # df = pd.concat([df, pd.DataFrame([nova_linha])], ignore_index=True)
-        # df.to_excel(caminho_arquivo, index=False, engine="openpyxl")
+        nova_linha = {
+            "Idade": idade,
+            "Altura": altura,
+            "Peso": peso,
+            "Gênero": genero,
+            "Distância real": real,
+            "Distância calculada": final_distance,
+        }
+        df = pd.concat([df, pd.DataFrame([nova_linha])], ignore_index=True)
+        df.to_excel(caminho_arquivo, index=False, engine="openpyxl")
 
         if repeats in [0,1]: 
             distances_right.append(final_distance)
@@ -499,8 +499,8 @@ while repeats < 4:
             side = "left"
 
 
-        # with open("./logs/logs_sit_and_reach2","a") as arquivo:
-        #     arquivo.write(f"{dt.datetime.now()}, {idade}, {altura}, {peso}, {genero}, {real}, {final_distance},{side}\n")
+        with open("./logs/logs_sit_and_reach2","a") as arquivo:
+            arquivo.write(f"{dt.datetime.now()}, {idade}, {altura}, {peso}, {genero}, {real}, {final_distance},{side}\n")
 
         final_repetition_visualization(final_distance)
 
