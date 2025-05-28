59de767a862736d706a160df05c63572915b9d4a
vgguerra <victorevandro94@gmail.com>
2025-05-28 14:43:16 +0100

    Salvando arquivos

--- runner.py
+++ runner.py
@@ -1,5 +1,6 @@
 import subprocess
 
+
 print("Executando Sit and Reach...")
 subprocess.run(["python", "./Sit-and-Reach/sit_and_reach_holistic_2.py"], check=True)
 
@@ -7,3 +8,4 @@ print("Executando Back Scratch...")
 subprocess.run(["python", "./Back-Scratch/back_scratch.py"], check=True)
 
 print("Todos os programas foram executados com sucesso.")
+ 
