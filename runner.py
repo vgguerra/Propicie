74e66858d462eabada6774d61261bb63453169d6
vgguerra <victorevandro94@gmail.com>
2025-05-30 15:09:17 +0100

    Testes com utentes dia 2

--- runner.py
+++ runner.py
@@ -2,8 +2,10 @@ import subprocess
 
 print("Executando Sit and Reach...")
 subprocess.run(["python", "./Sit-and-Reach/sit_and_reach_holistic_2.py"], check=True)
+subprocess.run(["python", "./Sit-and-Reach/sit_and_reach_holistic_2.py"], check=True)
 
 print("Executando Back Scratch...")
 subprocess.run(["python", "./Back-Scratch/back_scratch.py"], check=True)
+subprocess.run(["python", "./Back-Scratch/back_scratch.py"], check=True)
 
-print("Todos os programas foram executados com sucesso.")
+print("Testes concluídos com sucesso.")
