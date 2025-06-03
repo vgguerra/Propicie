18a10464d397dbf2ae8d726e3034882a5af8c30b
vgguerra <victorevandro94@gmail.com>
2025-06-03 22:17:33 +0100

    Análise dos dados capturados

---
+++ analises/analise.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+df = pd.read_excel('./tabelas_utentes/back_scratch_utentes.xlsx')  # Ou o caminho para a tabela referente ao Back-Scratch
+
+df.columns = df.columns.str.strip()
+
+agrupado = df.groupby(['Gender', 'Side'])['Erro'].agg(['mean', 'std', 'min', 'max'])
+
+agrupado.columns = ['Média', 'Desvio Padrão', 'Mínimo', 'Máximo']
+
+print(agrupado)
+
+agrupado.to_excel('./analises/estatisticas_erro_por_genero_e_lado_backscratch.xlsx')
+
+print(agrupado.to_latex())
